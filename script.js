f9ec0aaaab91a660b55c79a5b7fab062415e498b
vi <87@.ru>
2021-12-23 21:12:23 +0500

    finish

---
+++ script.js
@@ -0,0 +1,25 @@
+// let number = +prompt('Введите число');
+// let degree = +prompt('Введите степень');
+// let answer = 1;
+
+// for(let i = 0;i < degree;i++) {
+//  answer = number * answer;
+// }
+// alert(answer);
+
+
+
+
+let a = prompt ('введите максимальное количество ступенек');
+let b = prompt ('введите символы отступов');
+let c = prompt ('введите конечный символ');
+for(let i = 0;i < a;i++) {
+ if (i == 0) {
+     c
+}else{
+    c = b + c
+}
+console.log(c);
+}
+   
+
